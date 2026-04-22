071dea04bd6a57c1a87392b240fdc69932c27684
William Munene <williammunene2018@gmail.com>
2026-04-22 17:15:55 +0300

    cleaned up code

--- index.js
+++ index.js
@@ -4,17 +4,12 @@ function combineUsers(...args){
   let combinedObject={
     users: [],
   }
-  // for(let i=0; i<args.length; i++){
-  //   combinedObject.users=[...combinedObject.users, ...args[i]];
-  // }
   args.forEach(function(array){
     combinedObject.users=[...combinedObject.users, ...array];
   })
   combinedObject.merge_date=new Date().toString('M/d/yyyy');
   return combinedObject;
 }
-
-
 
 
 
